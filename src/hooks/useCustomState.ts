06fe8bf2ca28dbda154516504aec3f4bd4725917
Ildar <ildar.1795@gmail.com>
2024-07-17 13:19:45 +0400

    refactor: Создает отдельную константу для состояния visibleCount

--- src/hooks/useCustomState.ts
+++ src/hooks/useCustomState.ts
@@ -1,8 +1,10 @@
 import { useState } from "react"
 
+const VISIBLE_ELEMENTS_COUNT = 8
+
 export const useCustomState = (initialState: any) => {
 	const [state] = useState(initialState);
-	const [visibleCount, setVisibleCount] = useState(8);
+	const [visibleCount, setVisibleCount] = useState(VISIBLE_ELEMENTS_COUNT);
 	const [loading, setLoading] = useState(false);
 	let visibleState = state.slice(0, visibleCount);
 
