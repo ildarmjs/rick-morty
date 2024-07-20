8f5191f987b1fa95076c8a732d19a76593593b7a
Ildar <ildar.1795@gmail.com>
2024-07-20 19:26:23 +0400

    refactor: Прокидывает через пропсы ref, а так же имитирует ошибку, чтобы показать работу компонента ErrorBoundary

--- src/components/characters/characters-grid/characters-card/CharactersCard.tsx
+++ src/components/characters/characters-grid/characters-card/CharactersCard.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { forwardRef } from 'react'
 import { ICharacters } from '../../../../interfaces/characters'
 import { Link } from 'react-router-dom'
 
@@ -6,17 +6,23 @@ interface ICharactersCardProps {
 	character: ICharacters
 }
 
-const CharactersCard: FC<ICharactersCardProps> = ({ character }) => {
-	return (
-		<Link
-			to={`/characters/${character.id}`}
-			className='p-4 bg-zinc-700 shadow-inner rounded-lg'
-		>
-			<img src={character.image} alt={character.name} className='mb-[10px]' />
-			<div className='text-[18px] font-semibold'>{character.name}</div>
-			<div>{character.species}</div>
-		</Link>
-	)
-}
+const CharactersCard = forwardRef<HTMLAnchorElement, ICharactersCardProps>(
+	({ character }, ref) => {
+		if (character.id === 1 || character.id === 4) {
+			throw new Error('Искусственная ошибка в EpisodeCard')
+		}
+		return (
+			<Link
+				ref={ref}
+				to={`/characters/${character.id}`}
+				className='p-4 bg-zinc-700 shadow-inner rounded-lg'
+			>
+				<img src={character.image} alt={character.name} className='mb-[10px]' />
+				<div className='text-[18px] font-semibold'>{character.name}</div>
+				<div>{character.species}</div>
+			</Link>
+		)
+	}
+)
 
 export default CharactersCard
--- src/components/episodes/episode-card/EpisodeCard.tsx
+++ src/components/episodes/episode-card/EpisodeCard.tsx
@@ -1,22 +1,27 @@
-import { FC } from 'react'
+import { forwardRef } from 'react'
 import { IEpisodes } from '../../../interfaces/episodes'
 
 interface IEpisodeCardProps {
 	episode: IEpisodes
 }
 
-const EpisodeCard: FC<IEpisodeCardProps> = ({ episode }) => {
-	return (
-		<div className='p-4 bg-zinc-700 shadow-inner rounded-lg'>
-			<div className='text-[18px] font-semibold'>{episode.name}</div>
-			<div>
-				<span className='font-semibold'>Дата выхода:</span> {episode.air_date}
+const EpisodeCard = forwardRef<HTMLDivElement, IEpisodeCardProps>(
+	({ episode }, ref) => {
+		if (episode.id === 3) {
+			throw new Error('Искусственная ошибка в EpisodeCard')
+		}
+		return (
+			<div ref={ref} className='p-4 bg-zinc-700 shadow-inner rounded-lg'>
+				<div className='text-[18px] font-semibold'>{episode.name}</div>
+				<div>
+					<span className='font-semibold'>Дата выхода:</span> {episode.air_date}
+				</div>
+				<div>
+					<span className='font-semibold'>Эпизод:</span> {episode.episode}
+				</div>
 			</div>
-			<div>
-				<span className='font-semibold'>Эпизод:</span> {episode.episode}
-			</div>
-		</div>
-	)
-}
+		)
+	}
+)
 
 export default EpisodeCard
